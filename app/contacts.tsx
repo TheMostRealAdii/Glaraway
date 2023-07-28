24066976f1be865ebf47cb8a03d0688bfbe614df
Aditya Shah <82594256+ewwwadi@users.noreply.github.com>
2023-07-28 13:33:00 +0530

    int

---
+++ app/contacts.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import Link from "next/link";
+
+export default function Contact() {
+  return (
+    <>
+      <Head>
+        <title>Contact</title>
+      </Head>
+      <div></div>
+    </>
+  );
+}
